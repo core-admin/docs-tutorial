4cd9c77edb46679357ed535d37c2189cf755648f
xuke <xk1242425209@163.com>
2024-12-11 17:49:39 +0800

    Add SVG templates for various document types including blank document, business letter, cover letter, project proposal, resume, and software proposal. Implement a TemplatesGallery component to display these templates in a carousel format on the home page.

---
+++ src/constants/tempaltes.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: 'blank',
+    label: '空白文件',
+    imageUrl: '/templates/blank-document.svg',
+  },
+  {
+    id: 'software-proposal',
+    label: '软件开发提案',
+    imageUrl: '/templates/software-proposal.svg',
+  },
+  {
+    id: 'project-proposal',
+    label: '项目提案',
+    imageUrl: '/templates/project-proposal.svg',
+  },
+  {
+    id: 'business-letter',
+    label: '商务信函',
+    imageUrl: '/templates/business-letter.svg',
+  },
+  {
+    id: 'resume',
+    label: '简历',
+    imageUrl: '/templates/resume.svg',
+  },
+  {
+    id: 'cover-letter',
+    label: '求职信',
+    imageUrl: '/templates/cover-letter.svg',
+  },
+  {
+    id: 'letter',
+    label: '信件',
+    imageUrl: '/templates/letter.svg',
+  },
+];
